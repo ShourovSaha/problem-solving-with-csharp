98a5b1c7ab2cbcddf46e5be65f8e9dc07e368b1f
Shourov Saha <shourovsaha94@gmail.com>
2022-06-09 12:16:54 +0600

    added GFG "Subarray with given sum" problem. not yet solved.

---
+++ subarray_with_given_sum/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace subarray_with_given_sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int N = 5, S = 12;
+            int[] A = { 1, 2, 3, 7, 5 };
+            Solution solution = new Solution();
+
+            var result = solution.subarraySum(A, N, S);
+
+            result.ForEach(item => Console.Write(item + " "));
+
+            Console.ReadKey();
+
+        }
+    }
+
+
+    internal class Solution
+    {
+        //Function to find a continuous sub-array which adds up to a given number.
+        public List<int> subarraySum(int[] arr, int n, int s)
+        {
+            //code here
+            List<int> result = new List<int>();
+            int sumOfNumber = 0;
+            int stratPointer = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sumOfNumber += arr[i];
+                if (sumOfNumber == s)
+                {
+                    result.Add(stratPointer);
+                    result.Add(arr[i]);
+                }
+                else if(sumOfNumber > s)
+                {
+                    sumOfNumber -= arr[stratPointer];
+                    ++stratPointer;
+                }
+            }
+
+            if (sumOfNumber != s) result.Add(-1);
+
+            return result;
+        }
+
+    }
+}
